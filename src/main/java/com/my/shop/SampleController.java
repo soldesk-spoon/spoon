3ffd31acf86916b404f1a6ef339f9df918ce9853
ksj686 <kim_se_joon@naver.com>
2017-07-06 12:21:22 +0900

    게시판 쓰기 폼만

--- src/main/java/com/my/shop/SampleController.java
+++ src/main/java/com/my/shop/SampleController.java
@@ -4,15 +4,18 @@ import java.util.List;
 import java.util.Map;
  
 import javax.annotation.Resource;
- 
+
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
  
 @Controller
 public class SampleController {
-    Logger log = Logger.getLogger(this.getClass());
+    Logger logger = Logger.getLogger(this.getClass());
      
     @Resource(name="sampleService")
     private SampleService sampleService;
@@ -26,4 +29,23 @@ public class SampleController {
          
         return mv;
     }
+    
+	@RequestMapping(value="/sample/show_write_form.do", method=RequestMethod.GET)
+	public String show_write_form( Model model){
+		logger.info("show_write_form called!!");
+		model.addAttribute("boardBeanObjToWrite", new BoardBean());
+		
+		return "board/writeBoard";
+	}
+	/*
+	@RequestMapping(value="/DoWriteBoard.do", method=RequestMethod.POST)
+	public String DoWriteBoard(BoardBean boardBeanObjToWrite, Model model){		
+		logger.info("DoWriteBoard called!!");				
+		logger.info("memo=["+boardBeanObjToWrite.getMemo()+"]");
+		boardService.insertBoard(boardBeanObjToWrite);
+		model.addAttribute("totalCnt", new Integer(boardService.getTotalCnt()));
+		model.addAttribute("current_page", "1");
+		model.addAttribute("boardList", boardService.getList(1, 2));
+		return "board/listSpecificPage";		
+	}*/
 }
